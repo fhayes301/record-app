b104b2e703c869cef826b1138f11f09f297ebd40
Faye Hayes <fhayes30@gmail.com>
2017-09-15 18:14:59 -0700

    Build migrations and models. Seeded database with CSV file data

---
+++ db/migrate/20170916001529_create_records.rb
@@ -0,0 +1,14 @@
+class CreateRecords < ActiveRecord::Migration[5.1]
+  def change
+    create_table :records do |t|
+      t.integer :user_id
+      t.string :genre
+      t.string :artist
+      t.string :album
+      t.integer :year
+      t.integer :quantity
+
+      t.timestamps
+    end
+  end
+end
