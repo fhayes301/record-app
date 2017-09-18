abc55f82271f1a0f20b7a5900b06a360df1b5900
Faye Hayes <fhayes30@gmail.com>
2017-09-18 09:25:48 -0700

    Add users and sessions controller. Add Login/Logout functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :records
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  delete '/logout' => 'sessions#destroy'
+  
   root 'records#index'
 end
