ecb156293ad065c46e9f9672c0cc5cc7fdf8a25c
Faye Hayes <fhayes30@gmail.com>
2017-09-20 16:20:09 -0700

    Add Song model, controller, and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   resources :records
+  resources :songs
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
   delete '/logout' => 'sessions#destroy'
-  
+
   root 'records#index'
 end
