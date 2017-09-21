1d0d678ced44f179128ae76068b402d3f2d3428e
Faye Hayes <fhayes30@gmail.com>
2017-09-21 10:36:41 -0700

    Add RESTful methods to songs controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,15 +1,51 @@
 class SongsController < ApplicationController
+  def index
+    @record = Record.find(params[:record_id])
+    @song = @record.songs
+  end
+
+
   def new
+    @record = Record.find(params[:record_id])
     @song = Song.new
   end
 
   def create
-    @song = Song.new(song_params)
+    @record = Record.find(params[:record_id])
+    @song = @record.songs.new(song_params)
+
     if @song.save
-      redirect_to record_songs
+      redirect_to record_songs_path(@record)
+    else
+      render 'new'
+    end
   end
 
+  def edit
+    @record = Record.find(params[:record_id])
+  end
+
+  def update
+    @record = Record.find(params[:record_id])
+    @song = Song.find(params[:id])
+
+    if @song.update(song_params)
+      redirect_to record_songs_path(@record)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @record = Record.find(params[:record_id])
+    @record.destroy
+    redirect_to record_songs_path(@record)
+  end
 
   private
+
+  def song_params
+    params.require(:song).permit(:title, :record_id)
+  end
 
 end
